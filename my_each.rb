9bbb64657a719df996577541f12f170f3c655af1
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-25 19:50:08 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  i = 0 
+  while i < arr.length
+    yield(arr[i])
+    i += 1 
+  end
+  arr
 end
+
+my_each(arr) do |name|
+  puts name
+end
+
