6485583613904ab5d2ec4edc20f0e02787e23d3b
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-06 23:31:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,10 @@ def my_each(arr) # Declare method
   i = 0 # Set initial iteration value
   while i < arr.length # Set condition
     yield(arr[i]) # Will yield the specific iteration to a seperate block
+    
     i += 1 # Increments up by one
   end
-  p arr # Returns the arr array.
+  arr # Returns the arr array.
 end
 
 my_each(arr) do |name| # This is the yielded block
